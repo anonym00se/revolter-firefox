2e4333b0b14364cf78f32f86149c7c2e7ded205c
Xmader <xmader@outlook.com>
2019-07-12 17:14:06 -0400

    更新构建脚本

--- revolter-firefox-nss-build.sh
+++ revolter-firefox-nss-build.sh
@@ -7,19 +7,25 @@
 ./mach configure
 
 TOP_OBJDIR=$(ls -td obj-* | head -1)
-PYTHON_BIN="$TOP_OBJDIR/_virtualenvs/init/Scripts/python"
+PYTHON_BIN="$TOP_OBJDIR/_virtualenvs/init/Scripts/python"  # windows
+if [ ! -f "$PYTHON_BIN" ]; then
+    PYTHON_BIN="$TOP_OBJDIR/_virtualenvs/init/bin/python"  # *nix
+fi
 
 # header files in objdir
 $PYTHON_BIN ./init.py $TOP_OBJDIR
 $PYTHON_BIN build/variables.py buildid_header > "$TOP_OBJDIR/buildid.h"
 $PYTHON_BIN build/variables.py source_repo_header > "$TOP_OBJDIR/source-repo.h"
 
-mkdir "$TOP_OBJDIR/config/external/ffi/.deps"  # bug
+mkdir -p "$TOP_OBJDIR/config/external/ffi/.deps"  # bug
 
-timeout --signal=9 30s ./mach build  # bug
+timeout --signal=9 30s ./mach build || echo  # bug
 
 make -C "$TOP_OBJDIR" recurse_export
 
+./mach build ./config/external/nspr/pr/
+./mach build ./config/external/nspr/libc/
+./mach build ./config/external/nspr/ds/
 ./mach build ./security/nss/lib/certdb/certdb_certdb/
 ./mach build ./security/nss/lib/certhigh/certhigh_certhi/
 ./mach build ./security/nss/lib/cryptohi/cryptohi_cryptohi/
@@ -29,11 +35,8 @@ make -C "$TOP_OBJDIR" recurse_export
 ./mach build ./security/nss/lib/pk11wrap/pk11wrap_pk11wrap/
 ./mach build ./security/nss/lib/nss/nss_nss_static/
 ./mach build ./security/nss/lib/nss/nss_nss3_static/
-./mach build ./config/external/nspr/pr/
 ./mach build ./db/sqlite3/src/
 ./mach build ./security/nss/lib/util/util_nssutil/
-./mach build ./config/external/nspr/libc/
-./mach build ./config/external/nspr/ds/
 ./mach build ./security/nss/lib/pkcs12/pkcs12_pkcs12/
 ./mach build ./security/nss/lib/pkcs7/pkcs7_pkcs7/
 ./mach build ./security/nss/lib/smime/smime_smime/
