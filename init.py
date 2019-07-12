45fdfd152afd029553f12451f5c2fa0c64b240f7
Xmader <xmader@outlook.com>
2019-07-12 00:45:10 -0400

    添加自动构建 nss3.dll 的脚本

---
+++ init.py
@@ -0,0 +1,60 @@
+import re
+import os
+import sys
+
+args = sys.argv[1:]
+objdir = args[0] or os.environ.get("TOP_OBJDIR", ".")
+topdir = "."
+
+configpath = objdir + "/config.status"
+inputpath = topdir + "/mozilla-config.h.in"
+outputpath = objdir + "/mozilla-config.h"
+
+with open(configpath, 'rU') as configf:
+    lines = configf.readlines()
+    bindex = 826
+    eindex = 940
+    lines = lines[bindex:eindex]
+    jsondata = "{" + "".join(lines) + "}"
+    config = dict(eval(jsondata))  # unsafe
+
+output = []
+
+with open(inputpath, 'rU') as inputf:
+    inputlines = inputf.read().split("\n")
+    r = re.compile(
+        '^\s*#\s*(?P<cmd>[a-z]+)(?:\s+(?P<name>\S+)(?:\s+(?P<value>\S+))?)?', re.U)
+    for l in inputlines:
+        m = r.match(l)
+        if m:
+            cmd = m.group('cmd')
+            name = m.group('name')
+            value = m.group('value')
+            if name:
+                if name == 'ALLDEFINES':
+                    if cmd == 'define':
+                        raise Exception(
+                            '`#define ALLDEFINES` is not allowed in a '
+                            'CONFIGURE_DEFINE_FILE')
+
+                    def define_for_name(name, val):
+                        """WebRTC files like to define WINVER and _WIN32_WINNT
+                        via the command line, which raises a mass of macro
+                        redefinition warnings.  Just handle those macros
+                        specially here."""
+                        define = "#define {name} {val}".format(
+                            name=name, val=val)
+                        if name in ('WINVER', '_WIN32_WINNT'):
+                            return '#if !defined({name})\n{define}\n#endif' \
+                                .format(name=name, define=define)
+                        return define
+                    defines = '\n'.join(sorted(
+                        define_for_name(name, val)
+                        for name, val in config.iteritems()))
+                    l = l[:m.start('cmd') - 1] \
+                        + defines + l[m.end('name'):]
+
+        output.append(l)
+
+with open(outputpath, 'wb+') as outputf:
+    outputf.write("\n".join(output))
---
+++ revolter-firefox-nss-build.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+# clean
+./mach clobber
+
+# init
+./mach configure
+
+TOP_OBJDIR=$(ls -td obj-* | head -1)
+PYTHON_BIN="$TOP_OBJDIR/_virtualenvs/init/Scripts/python"
+
+# header files in objdir
+$PYTHON_BIN ./init.py $TOP_OBJDIR
+$PYTHON_BIN build/variables.py buildid_header > "$TOP_OBJDIR/buildid.h"
+$PYTHON_BIN build/variables.py source_repo_header > "$TOP_OBJDIR/source-repo.h"
+
+mkdir "$TOP_OBJDIR/config/external/ffi/.deps"  # bug
+
+timeout --signal=9 15s ./mach build  # bug
+
+make -C "$TOP_OBJDIR" recurse_export
+
+./mach build ./security/nss/lib/certdb/certdb_certdb/
+./mach build ./security/nss/lib/certhigh/certhigh_certhi/
+./mach build ./security/nss/lib/cryptohi/cryptohi_cryptohi/
+./mach build ./security/nss/lib/base/base_nssb/
+./mach build ./security/nss/lib/dev/dev_nssdev/
+./mach build ./security/nss/lib/pki/pki_nsspki/
+./mach build ./security/nss/lib/pk11wrap/pk11wrap_pk11wrap/
+./mach build ./security/nss/lib/nss/nss_nss_static/
+./mach build ./security/nss/lib/nss/nss_nss3_static/
+./mach build ./config/external/nspr/pr/
+./mach build ./db/sqlite3/src/
+./mach build ./security/nss/lib/util/util_nssutil/
+./mach build ./config/external/nspr/libc/
+./mach build ./config/external/nspr/ds/
+./mach build ./security/nss/lib/pkcs12/pkcs12_pkcs12/
+./mach build ./security/nss/lib/pkcs7/pkcs7_pkcs7/
+./mach build ./security/nss/lib/smime/smime_smime/
+./mach build ./security/nss/lib/smime/smime_smime3_static/
+./mach build ./security/nss/lib/ssl/ssl_ssl/
+./mach build ./memory/build/
+./mach build ./memory/mozalloc/
+./mach build ./mozglue/misc/
+./mach build ./ipc/mscom/mozglue/
+./mach build ./mozglue/build/
+./mach build ./security/nss/
